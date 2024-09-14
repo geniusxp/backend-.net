b9df76d646c9ae49ad05ae36496c90769e34f604
Lucas Araujo <lucasaraujo4188@gmail.com>
2024-09-13 21:30:48 -0300

    deploy api

--- Program.cs
+++ Program.cs
@@ -30,12 +30,17 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+
+app.UseSwaggerUI();
+
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
